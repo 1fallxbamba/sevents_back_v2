6910e68d310ab336b11df793af89ce1517fd0325
Bamba Fall <fbamba99@gmail.com>
2022-03-27 21:23:07 +0000

    config: typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PWD,
+      database: process.env.PG_DB,
+      autoloadEntities: true,
+      synchronise: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
