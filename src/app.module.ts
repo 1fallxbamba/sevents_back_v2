0570c324dbf5941af993b9d62137db02d8b630bf
Bamba Fall <fbamba99@gmail.com>
2022-03-29 15:38:45 +0000

    feature: serve static images to public

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeventModule } from './sevent/sevent.module';
-
 import { ormConfig } from './config/ormconfig';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     MulterModule.register({ dest: './public/images' }),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(ormConfig),
