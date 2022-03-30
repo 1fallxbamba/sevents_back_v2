6776c3abb97f688a67aa61ea83de7a2822c8ab8e
Bamba Fall <fbamba99@gmail.com>
2022-03-30 19:50:44 +0000

    feature: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v2');
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
